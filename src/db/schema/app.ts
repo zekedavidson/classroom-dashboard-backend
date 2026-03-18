7e88cd54447bb89da3f3b40abd96ae9e9b38f6d1
zekedavidson <bhatiagary28@gmail.com>
2026-03-18 12:21:16 +0530

    demo index.ts removed

--- src/db/schema/app.ts
+++ src/db/schema/app.ts
@@ -35,5 +35,5 @@ export const subjectsRelations = relations(subjects, ({ one, many }) => ({
 export type Department = typeof departments.$inferSelect;
 export type NewDepartment = typeof departments.$inferInsert;
 
-export type Subject = typeof departments.$inferSelect;
-export type NewSubject = typeof departments.$inferInsert;
+export type Subject = typeof subjects.$inferSelect;
+export type NewSubject = typeof subjects.$inferInsert;
