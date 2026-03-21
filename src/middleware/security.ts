b39a4ba3e0dca4d441d59b454898dbefecc03a16
zekedavidson <bhatiagary28@gmail.com>
2026-03-21 15:33:28 +0530

    changed status code

--- src/middleware/security.ts
+++ src/middleware/security.ts
@@ -54,7 +54,7 @@ const securityMiddleware = async (req: Request, res: Response, next: NextFunctio
         }
 
         if (decision.isDenied() && decision.reason.isRateLimit()) {
-            return res.status(403).json({ error: 'Too many request', message });
+            return res.status(429).json({ error: 'Too many request', message });
         }
 
         next();
