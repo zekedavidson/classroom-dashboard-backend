778f8f1f6d645e735d051fd8265d10cf2626c71f
zekedavidson <bhatiagary28@gmail.com>
2026-03-24 22:01:18 +0530

    add file extensions

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import express from 'express';
 import cors from "cors";
 import subjectsRouter from './routes/subjects.js';
 import securityMiddleware from './middleware/security.js';
-import { auth } from './lib/auth';
+import { auth } from './lib/auth.js';
 import { toNodeHandler } from 'better-auth/node';
 
 const app = express();
