9b2b711088aa41c858bea18211d476d3550c0911
zekedavidson <bhatiagary28@gmail.com>
2026-03-25 14:40:01 +0530

    implement class details

--- src/routes/classes.ts
+++ src/routes/classes.ts
@@ -1,26 +1,12 @@
 import express from "express";
 import { db } from "../db/index.js";
-import { classes, subjects, user } from "../db/schema/index.js";
+import { classes, departments, subjects } from "../db/schema/app.js";
+import { user } from "../db/schema/auth.js";
 import { and, desc, eq, getTableColumns, ilike, or, sql } from "drizzle-orm";
 
 const router = express.Router();
 
-router.post('/', async (req, res) => {
-    try {
-        const [createdClass] = await db
-            .insert(classes)
-            .values({ ...req.body, inviteCode: Math.random().toString(36).substring(2, 7), schedules: [] })
-            .returning({ id: classes.id });
-
-        if (!createdClass) throw Error;
-
-        res.status(201).json({ data: createdClass });
-    } catch (e) {
-        console.log(`POST /classes error ${e}`);
-        res.status(500).json({ error: e })
-    }
-})
-
+// Get all classes with optional search, filtering and pagination
 router.get("/", async (req, res) => {
     try {
         const { search, teacher, subject, page = 1, limit = 10 } = req.query;
@@ -89,6 +75,52 @@ router.get("/", async (req, res) => {
     } catch (e) {
         console.log(`GET /classes error: ${e}`);
         res.status(500).json({ error: 'Failed to get subjects ' });
+    }
+})
+
+// Get class details wtih teacher, subject and department
+router.get('/:id', async (req, res) => {
+    const classId = Number(req.params.id);
+
+    if (!Number.isFinite(classId)) return res.status(400).json({ error: 'No class found' });
+
+    const [classDetails] = await db
+        .select({
+            ...getTableColumns(classes),
+            subject: {
+                ...getTableColumns(subjects),
+            },
+            department: {
+                ...getTableColumns(departments)
+            },
+            teacher: {
+                ...getTableColumns(user)
+            }
+        })
+        .from(classes)
+        .leftJoin(subjects, eq(classes.subjectId, subjects.id))
+        .leftJoin(user, eq(classes.teacherId, user.id))
+        .leftJoin(departments, eq(subjects.departmentId, departments.id))
+        .where(eq(classes.id, classId))
+
+    if (!classDetails) return res.status(404).json({ error: 'No class found' });
+
+    res.status(200).json({ data: classDetails })
+})
+
+router.post('/', async (req, res) => {
+    try {
+        const [createdClass] = await db
+            .insert(classes)
+            .values({ ...req.body, inviteCode: Math.random().toString(36).substring(2, 7), schedules: [] })
+            .returning({ id: classes.id });
+
+        if (!createdClass) throw Error;
+
+        res.status(201).json({ data: createdClass });
+    } catch (e) {
+        console.log(`POST /classes error ${e}`);
+        res.status(500).json({ error: e })
     }
 })
 
